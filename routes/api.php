c8844934cae5fcf52ffa56b0ab8e2d6af64d1a64
Shaikh Imran <imranshkh283@gmail.com>
2025-05-23 20:29:21 +0530

    route : prefix category

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,11 @@ Route::post('/login', [AuthController::class, 'index'])->name('login');
 // Resource routes of category
 Route::middleware(['auth:sanctum'])->group(function () {
 
-    Route::middleware('admin')->group(function () {
-        Route::get('/categories', [CategoryController::class, 'index']);
-        Route::post('/categories/create', [CategoryController::class, 'create']);
-        Route::patch('/categories/{id}', [CategoryController::class, 'update']);
-        Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
+    Route::middleware('admin')->prefix('category')->group(function () {
+        Route::get('/', [CategoryController::class, 'index']);
+        Route::post('/create', [CategoryController::class, 'create']);
+        Route::patch('/{id}', [CategoryController::class, 'update']);
+        Route::delete('/{id}', [CategoryController::class, 'delete']);
     });
 
     Route::get('/articles', [ArticleController::class, 'index']);
